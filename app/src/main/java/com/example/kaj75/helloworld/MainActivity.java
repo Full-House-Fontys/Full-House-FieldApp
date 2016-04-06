028608bdf047f7170479e7d7fac3e93e0bee15e5
Skaj7 <kaj753@hotmail.com>
2016-04-06 10:29:08 +0200

    remove button

--- app/src/main/java/com/example/kaj75/helloworld/MainActivity.java
+++ app/src/main/java/com/example/kaj75/helloworld/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //werkt
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
